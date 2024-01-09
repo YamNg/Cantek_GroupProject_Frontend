ba71a5dec0d9479134fd89b1ad04279a1b9a0067
Yam Ng <108157094+YamNg@users.noreply.github.com>
2024-01-09 15:23:47 -0500

    add initial configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import store from "./store";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Could not find root element");
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
