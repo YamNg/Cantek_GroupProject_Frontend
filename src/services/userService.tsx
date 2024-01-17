9e4c44a419df1931c6a5bcf3d5c9c5fa635a0d82
Yam Ng <108157094+YamNg@users.noreply.github.com>
2024-01-16 22:47:12 -0500

    trial for solving user token cookies issue

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -3,6 +3,7 @@ import { IApiResponse } from "../models/api/api-response.api.interface";
 import { IUser } from "../models/api/user.api.interface";
 
 const baseUrl = import.meta.env.VITE_USER_BASE_URL;
+const currentFrontendUrl = import.meta.env.VITE_CURRENT_FRONTEND_URL;
 
 const login = async ({
   email,
@@ -11,7 +12,12 @@ const login = async ({
   email: string;
   password: string;
 }) => {
-  const requestUrl = `${baseUrl}/login`;
+  // for solving the issue which using public solution (e.g. Render) to host the application
+  // which Cookie cannot set to the public recognized domain
+  // Current solution is to define a Rewrite rule in Render, to simulate requesting login api with same domain
+  // and bypass the error for setting the cookies for login tokens
+  let requestUrl = currentFrontendUrl ? currentFrontendUrl : baseUrl;
+  requestUrl = `${requestUrl}/login`;
 
   const { data }: { data: IApiResponse<IUser> } = await axios.post(
     requestUrl,
@@ -22,7 +28,12 @@ const login = async ({
 };
 
 const logout = async ({ userId }: { userId: string }) => {
-  const requestUrl = `${baseUrl}/logout`;
+  // for solving the issue which using public solution (e.g. Render) to host the application
+  // which Cookie cannot set to the public recognized domain
+  // Current solution is to define a Rewrite rule in Render, to simulate requesting login api with same domain
+  // and bypass the error for setting the cookies for login tokens
+  let requestUrl = currentFrontendUrl ? currentFrontendUrl : baseUrl;
+  requestUrl = `${requestUrl}/logout`;
 
   const { data }: { data: IApiResponse<IUser> } = await axios.post(
     requestUrl,
